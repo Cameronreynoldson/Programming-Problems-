5ca4333ba317093c997c11bec99045330f5b1609
Cameron Reynoldson <Cameronreynoldson@utexas.edu>
2015-08-20 22:57:50 -0500

    Update EditDistance.java

--- EditDistance.java
+++ EditDistance.java
@@ -1,45 +1,39 @@
-import java.util.*; 
-public class EditDistance
-{
-	public static void main(String[] args)
-	{
-		Scanner scanner = new Scanner(System.in);
-		String s1 = scanner.next();
-		String s2 = scanner.next();
-		solve(s1,s2);
-	}
-
-	private static void solve(String s1, String s2)
-	{
-		int m = s1.length();
-		int n = s2.length();
-
-		int[][] matrix = new int[m+1][n+1];
-
-		for(int i = 1; i <=m; ++i)
-		{
-			matrix[i][0] = i;
-		}
-
-		for(int i = 1; i <= n; ++i)
-		{
-			matrix[0][i] = i;
-		}
-
-		for(int i = 1; i <= m; ++i)
-		{
-			for(int j = 1; j <= n; ++j)
-			{
-				int difference = (s1.charAt(i) == s2.charAt(j)) ? 0: 1;
-				matrix[i][j] = min(matrix[i-1][j]+1,matrix[i][j-1]+1,difference);
-			}
-		}
-
-		System.out.println(matrix[m][n]);
-	}
-
-	private static int min(int a, int b, int c)
-	{
-		return (a < b) ? Math.min(a,c) : Math.min(b,c);
-	}
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        if(word1.length() == 0 || word2.length() == 0) return Math.max(word1.length(),word2.length()); 
+        int[][] matrix = new int[word1.length()+1][word2.length()+1]; 
+        for(int i = 0; i < word1.length(); ++i)
+        {
+            matrix[i][0] = i;
+        }
+        
+        for(int i = 0; i < word2.length(); ++i)
+        {
+            matrix[0][i] = i;
+        }
+        
+        
+        for(int i = 1; i <= word1.length(); ++i)
+        {
+            for(int j = 1; j <= word2.length(); ++j)
+            {
+                
+                if(word1.charAt(i-1) == word2.charAt(j-1))
+                {
+                    matrix[i][j] = matrix[i-1][j-1];
+                }
+                else
+                {
+                    matrix[i][j] = min(matrix[i-1][j]+1,matrix[i][j-1]+1,matrix[i-1][j-1]+1);   
+                }
+            }
+        }
+        
+        return matrix[word1.length()][word2.length()];
+    }
+    
+    public int min(int a, int b, int c)
+    {
+        return (a < b) ? Math.min(a,c) : Math.min(b,c);
+    }
 }
