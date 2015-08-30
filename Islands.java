42252ad6f64309ef4d1acbf96b7847076b69f2b0
Cameron Reynoldson <Cameronreynoldson@utexas.edu>
2015-08-30 00:00:11 -0500

    Update Islands.java

--- Islands.java
+++ Islands.java
@@ -18,13 +18,15 @@ public class Islands {
     private static void dfs(char[][] grid, int i, int j)
     { 
         if(i >= 0 && j >= 0 && i < grid.length && j < grid[0].length)
-        if(grid[i][j] == '1')
         {
-            grid[i][j] = '0';
-            dfs(grid,i+1,j);
-            dfs(grid,i-1,j);
-            dfs(grid,i,j-1);
-            dfs(grid,i,j+1);
+            if(grid[i][j] == '1')
+            {
+                grid[i][j] = '0';
+                dfs(grid,i+1,j);
+                dfs(grid,i-1,j);
+                dfs(grid,i,j-1);
+                dfs(grid,i,j+1);
+            }
         }
     }
 }
