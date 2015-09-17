15af90c5c7ed05984a901fa63057ae6923b3638b
Cameron Reynoldson <Cameronreynoldson@utexas.edu>
2015-09-16 23:56:27 -0500

    Update IsBSTInOrder.java

--- IsBSTInOrder.java
+++ IsBSTInOrder.java
@@ -1,57 +1,23 @@
-import java.util.*; 
-
-public class IsBSTInOrder
-{
-	public static void main(String[] args)
-	{
-
-		int[] prev = new int[1];
-		Node root = new Node(5);
-		root.left = new Node(3);
-		root.right = new Node(10);
-		System.out.println(isBST(prev,root,false));
-
-	}
-
-	public static boolean isBST(int[] prev, Node node, boolean started)
+public static boolean isBST( Node node, int[] prev, boolean[] started)
 	{
 		if(node == null)
 		{
-			return true; 
+			return true;
+		}
+		if(!isBST(node.left, prev, started))
+		{
+			return false;
+		}
+		if(!started[0])
+		{
+			started[0] = true;
+		}
+		else if (node.value <= prev[0])
+		{
+			return false;
 		}
 
-		if(!isBST(prev,node.left,started))
-		{
-			return false; 
-		}
-
-		if(started && prev[0] >= node.value)
-		{
-			return false; 
-		}
-		else
-		{
-			prev[0] = node.value;
-			started = true; 
-		}
-		
-		if(!isBST(prev,node.right,started))
-		{
-			return false; 
-		}
-
-		return true; 
+		prev[0] = node.value;
+		return isBST(node.right, prev, started));
 	}	
-}
-
-class Node
-{
-	int value; 
-	Node left; 
-	Node right; 
-
-	public Node(int v)
-	{
-		value = v; 
-	}
 }
